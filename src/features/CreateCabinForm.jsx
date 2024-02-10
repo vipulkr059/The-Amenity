40dd7363a5e6d6c3a19402b4e33fad468d9a4a96
vipulkr059 <vipulkr059@gmail.com>
2024-02-10 14:43:30 +0530

    refactor : refactored Cabin form

--- src/features/CreateCabinForm.jsx
+++ src/features/CreateCabinForm.jsx
@@ -1,50 +1,15 @@
-import styled from "styled-components";
+import toast from "react-hot-toast";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+
 import Input from "../ui/Input";
 import Form from "../ui/Form";
 import Button from "../ui/Button";
 import FileInput from "../ui/FileInput";
 import { Textarea } from "../ui/Textarea";
 import FormRow from "../ui/FormRow";
-import { useForm } from "react-hook-form";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
 import { createCabin } from "../services/apiCabins";
-import toast from "react-hot-toast";
-
-// const FormRow = styled.div`
-//   display: grid;
-//   align-items: center;
-//   grid-template-columns: 24rem 1fr 1.2fr;
-//   gap: 2.4rem;
-
-//   padding: 1.2rem 0;
-
-//   &:first-child {
-//     padding-top: 0;
-//   }
-
-//   &:last-child {
-//     padding-bottom: 0;
-//   }
-
-//   &:not(:last-child) {
-//     border-bottom: 1px solid var(--color-grey-100);
-//   }
-
-//   &:has(button) {
-//     display: flex;
-//     justify-content: flex-end;
-//     gap: 1.2rem;
-//   }
-// `;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
 
 function CreateCabinForm() {
   const queryClient = useQueryClient();
