ada8c7cbf9c991c3f104570424392dfb042eeca4
vipulkr059 <vipulkr059@gmail.com>
2024-02-16 23:44:12 +0530

    feat: Dashboard completed, chart and activities added  :sparkles:

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,9 @@ import { useRecentStays } from "./useRecentStays";
 import { useCabins } from "../cabins/useCabins";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
+import TodayActivity from "./../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -31,9 +34,9 @@ export default function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today Activity</div>
-      <div>Chart stays</div>
-      <div>Chart Sales</div>
+      <TodayActivity />
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -14,6 +14,8 @@ const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
   overflow: scroll;
+  overflow-y: scroll;
+  overflow-x: hidden;
 `;
 const Container = styled.div`
   max-width: 120rem;
