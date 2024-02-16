0749bd9e34d702fbf6fe1c60a82a798701fa7d01
vipulkr059 <vipulkr059@gmail.com>
2024-02-16 20:42:42 +0530

    feat: User Info and Password update added  :sparkles:

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -1,12 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateCurrentUser as updateUserApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
 export function useUpdateUser(params) {
+  const queryClient = useQueryClient();
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateUserApi,
-    onSuccess: () => {
+    onSuccess: ({ user }) => {
       toast.success("User info updated successfully");
+      queryClient.setQueryData(["user"], user);
     },
     onError: (err) => {
       toast.error(err.message);
